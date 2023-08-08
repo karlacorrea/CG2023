ffad14b474d7ec85c7efe79292f69c50c1fcae18
karlacorrea <47987828+karlacorrea@users.noreply.github.com>
2023-08-08 18:00:28 -0300

    Add files via upload

---
+++ aula 02.java
@@ -0,0 +1,123 @@
+ATIVIDADES CG
+
+AULA 2 -
+A) 02 - Colors & Variables
+(Cores & Variaveis)
+
+// setup only gets run once
+void setup () {
+	size(800, 600);
+}
+
+// draw happens every frame!
+
+void draw () {
+	rect(mouseX, mouseY, 100, 100);
+}
+
+B) 02 - Colors & Variables
+(Cores & Variaveis)
+
+// setup only gets run once
+void setup () {
+	size(800, 600);
+	
+}
+
+// draw happens every frame!
+
+void draw () {
+	fill(255, 0, 0);
+	rect(mouseX, mouseY, 100, 100);
+}
+
+C) 02 - Colors & Variables
+(Cores & Variaveis)
+
+// setup only gets run once
+void setup () {
+	size(800, 600);
+	
+}
+
+// draw happens every frame!
+
+void draw () {
+	noStroke();
+	fill(255, 0, 0);
+	rect(mouseX, mouseY, 100, 100);
+}
+
+
+C) 02 - Colors & Variables
+(Cores & Variaveis)
+
+// setup only gets run once
+void setup () {
+	size(800, 600);
+	
+}
+
+// draw happens every frame!
+
+void draw () {
+	noStroke();
+	fill(random(255), 0, 0);
+	rect(mouseX, mouseY, 100, 100);
+}
+
+C) 02 - Colors & Variables
+(Cores & Variaveis)
+
+// setup only gets run once
+void setup () {
+	size(800, 600);
+	
+}
+
+// draw happens every frame!
+
+void draw () {
+	noStroke();
+	fill(random(255), 255, random(255);
+	rect(mouseX, mouseY, 100, 100);
+}
+
+
+
+D) 02 - Colors & Variables
+(Cores & Variaveis)
+
+// setup only gets run once
+void setup () {
+	size(800, 600);
+	
+}
+
+// draw happens every frame!
+
+void draw () {
+	noStroke();
+	fill(random(255), random(255), 255);
+	rect(mouseX, mouseY, 100, 100);
+}
+
+E) 02 - Colors & Variables
+(Cores & Variaveis)
+
+// setup only gets run once
+void setup () {
+	size(800, 600);
+	
+}
+
+// draw happens every frame!
+
+void draw () {
+	noStroke();
+	fill(random(255), random(255), 255);
+	ellipse(mouseX, mouseY, 100, 100);
+}
+
+
+
